efa44b9cb82685674286e8a7d26a6d06bb6791a4
adithyasource <adithyasrc@gmail.com>
2026-07-31 22:40:11 +0530

    cleaning stuf

--- scripts/smart-analysis.sh
+++ scripts/smart-analysis.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
-set -eu
+DEVICE="/dev/sda"
+OUT="/home/akina/.logs/smart"
 
-smartctl -t long /dev/sda
+SMART="$(smartctl -a "$DEVICE")"
 
-echo "$(date '+%F %T') | OK | smart test initiated" >> /home/akina/.logs/smart
+health=$(printf "%s\n" "$SMART" | awk -F': ' '/SMART overall-health self-assessment test result/ {print $2}')
+temp=$(printf "%s\n" "$SMART" | awk '$2=="Temperature_Celsius"{print $10}')
+hours=$(printf "%s\n" "$SMART" | awk '$2=="Power_On_Hours"{print $10}')
+realloc=$(printf "%s\n" "$SMART" | awk '$2=="Reallocated_Sector_Ct"{print $10}')
+uncorrect=$(printf "%s\n" "$SMART" | awk '$2=="Reported_Uncorrect"{print $10}')
+crc=$(printf "%s\n" "$SMART" | awk '$2=="UDMA_CRC_Error_Count"{print $10}')
+selftest=$(printf "%s\n" "$SMART" | awk '/^# 1/{print $3" "$4" "$5}')
+
+printf "%s | %s | %d°C | %dh | Realloc:%s Uncorrect:%s CRC:%s\n" \
+    "$(date '+%Y-%m-%d %H:%M:%S')" \
+    "$health" \
+    "$temp" \
+    "$hours" \
+    "$realloc" \
+    "$uncorrect" \
+    "$crc" \
+    >> "$OUT"
